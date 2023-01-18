4620dca108a283209fc7c3df5bc406e1fcb00f7b
Renzo Kooi <renzo.kooi@gmail.com>
2023-01-18 12:43:07 +0100

    tests DRY

--- basictests.js
+++ basictests.js
@@ -1,6 +1,7 @@
 import factory from "./datefiddler.js";
-import assert from 'node:assert/strict';
+import {strictEqual, deepStrictEqual, doesNotThrow} from 'node:assert/strict';
 runTests();
+process.exit(0);
 
 function runTests() {
   const xDate = factory(exts);
@@ -13,323 +14,134 @@ function runTests() {
   const getLine = err => `\n   @line ${err.stack.split(`\n`)[1].split(`:`).slice(-2, -1).shift()}\n`;
   const logs = [];
   let errors = false;
+  let cnt = 0;
   const log = str => logs.push(`  ${str.replace(/\n/, `\n  `)}`);
+  const runTest = (value, expected, okLine, testFn2Run = strictEqual) => {
+    cnt += 1;
+    try {
+      testFn2Run(value instanceof Function ? value() : value, expected, okLine);
+      log(`   test ${okLine} OK`);
+    } catch (err) {
+      errors = true;
+      log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
+    }
+  };
 
   log(`\nNote: formatter = ${formatter.toString()}`);
   log(`\n--- instances ---`);
   log(`\nwd = xDate(new Date(), formatter) => ${wd.value}`);
-
-  try {
-    assert.strictEqual(wd.ISO, d.toISOString(), "wd date");
-    log("   test wd date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(wd.value, formatter(d), "wd value");
-    log("   test wd value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
+  runTest(wd.ISO, d.toISOString(), `wd [date]`);
+  runTest(wd.value, formatter(d), `wd [value]`);
   log(`\ndsr = wd.clone(formatter).set(new Date(2000, 0 , 1)) => ${dsr.value}`);
-
-  try {
-    assert.strictEqual(dsr.ISO, dsrClone.toISOString(), "dsr = wd clone and set date");
-    log("   test wd clone and set date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(dsr.value, formatter(dsrClone), "dsr = wd clone and set value");
-    log("   test dsr = wd clone and set value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(dsr.ISO, dsrClone.toISOString(), `dsr = wd clone and set [date]`);
+  runTest(dsr.value, formatter(dsrClone), `dsr = wd clone and set [value]`);
 
   log(`\n--- methods ---`);
 
   dsr.set(new Date(2000, 1, 1));
   log(`\ndsr.set(new Date(2000, 1 , 1) => ${dsr.value}`);
-
-  try {
-    assert.strictEqual(dsr.ISO, new Date(2000, 1, 1).toISOString(), "dsr.set date");
-    log("   test dsr.set date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(dsr.value, formatter(new Date(2000, 1, 1)), "dsr.set value");
-    log("   test dsr.set value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(dsr.ISO, new Date(2000, 1, 1).toISOString(), `dsr.set [date]`);
+  runTest(dsr.value, formatter(new Date(2000, 1, 1)), `dsr.set [value]`);
 
   dsr.add(`2 days`);
   log(`\ndsr.add(\`2 days\`) => ${dsr.value}`);
-
-  try {
-    assert.strictEqual(dsr.ISO, new Date(2000, 1, 3).toISOString(), "dsr.add date");
-    log("   test dsr.add date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(dsr.ISO, new Date(2000, 1, 3).toISOString(), `dsr.add [date]`);
 
   log(`\ndsr.reset() => ${dsr.reset().value}`)
-
-  try {
-    assert.deepStrictEqual(dsr.date, dsr.initial, "dsr.reset value");
-    log("   test dsr.reset value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(dsr.date, dsr.initial, `dsr.reset [date]`, deepStrictEqual);
 
   log(`\n--- exensions ---`);
+
   let newDate = new Date(wd.date);
   newDate.setDate(newDate.getDate() + 7);
   wd.nextWeek();
   log(`\nwd.nextWeek() => ${wd.value}`);
-
-  try {
-    assert.strictEqual(wd.ISO, newDate.toISOString(), "wd.nextWeek date");
-    log("   test wd.nextWeek date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(wd.value, formatter(newDate), "wd.nextWeek value");
-    log("   test wd.nextWeek value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(wd.ISO, newDate.toISOString(), `wd.nextWeek [date]`);
+  runTest(wd.value, formatter(newDate), `wd.nextWeek [value]`);
 
   newDate = new Date(wd.date);
   newDate.setMonth(newDate.getMonth() - 2);
   wd.previousMonth().previousMonth();
-
   log(`\n--- chain extension methods ---`);
   log(`\nwd.previousMonth().previousMonth() => ${wd.value}`);
-
-  try {
-    assert.strictEqual(wd.ISO, newDate.toISOString(), "wd.previousMonth (2x) (double) date");
-    log("   test wd.previousMonth (2x) date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(wd.value, formatter(newDate), "wd.previousMonth (2x) value");
-    log("   test wd.previousMonth (2x) value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(wd.ISO, newDate.toISOString(), `wd.previousMonth (2x) (double) [date]`);
+  runTest(wd.value, formatter(newDate), `wd.previousMonth (2x) (double) [value]`);
 
   log(`\n--- chain extension methods and "native" methods ---`);
+
   newDate = new Date(wd.date);
   newDate.setDate(newDate.getDate() - 1);
   newDate.setFullYear(newDate.getFullYear() + 3);
   wd.yesterday().add(`3 years`);
   log(`\nwd.yesterday().add(\`3 years\`) => ${wd.value}`);
-
-  try {
-    assert.strictEqual(wd.ISO, newDate.toISOString(), "wd.yesterday.add date");
-    log("   test wd.yesterday.add date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}     expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(wd.value, formatter(newDate), "wd.yesterday.add value");
-    log("   test wd.yesterday.add value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(wd.ISO, newDate.toISOString(), `wd.yesterday.add [date]`);
+  runTest(wd.value, formatter(newDate), `wd.yesterday.add [value]`);
 
   log(`\n--- reset instance date to its initial date ---`);
   log(`\nwd.value => current value ${wd.value}`)
   wd.reset();
   log(`wd.reset().value => new value ${wd.value}`);
-
-  try {
-    assert.strictEqual(wd.ISO, wd.initial.toISOString(), "wd.reset (all) date");
-    log("   test wd.reset date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(wd.value, formatter(wd.initial), "wd.reset value");
-    log("   test wd.reset date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(wd.ISO, wd.initial.toISOString(), `wd.reset (initial) [date]`);
+  runTest(wd.value, formatter(wd.initial), `wd.reset (initial) [value]`);
 
   log(`\n--- reset date *and* initial date to current instance date value ---`);
+
   wd.set(new Date(2021, 7, 27));
   wd.update2Current();
   log(`\nwd.set(new Date(2021, 7, 27)); => ${wd.value}`);
   log(`wd.update2Current() => ${wd.value}`);
-
-  try {
-    assert.strictEqual(wd.ISO, wd.initial.toISOString(), "wd.reset (all) date");
-    log("   test wd.reset (all) date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(wd.value, formatter(wd.initial), "wd.reset (all) value");
-    log("   test wd.reset (all) value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(wd.ISO, wd.initial.toISOString(), `wd.reset (all) [date]`);
+  runTest(wd.value, formatter(wd.initial), `wd.reset (all) [value]`);
 
   log(`\n--- reset all using [instance].set ---`);
+
   const ddr = new Date();
   dsr.set(ddr, true);
   log(`\ndsr.set(new Date(), true) => ${dsr.value}`);
-
-  try {
-    assert.strictEqual(dsr.ISO, ddr.toISOString(), "dsr.set (all) date");
-    log("   test dsr.reset (all) date OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
-
-  try {
-    assert.strictEqual(dsr.value, formatter(dsr), "dsr.reset (all) value");
-    log("   test dsr.reset (all) value OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(dsr.ISO, ddr.toISOString(), `dsr.set (all) [date]`);
+  runTest(dsr.value, formatter(dsr), `dsr.set (all) [value]`);
 
   log(`\n-- .add parameters ---`);
+
   const paramsAsCommaDelimitedString = xDate(new Date(1900, 0, 1)).add("1 day, 2 hours, 30 minutes");
   log(`\nconst paramsAsCommaDelimitedString = xDate(new Date(1900, 0, 1)).add("1 day, 2 hours, 30 minutes");
      => ${paramsAsCommaDelimitedString.value}`);
-
-  try {
-    assert.deepEqual(paramsAsCommaDelimitedString.date, new Date(1900, 0, 2, 2, 30), ".add single string comma delimited parameter");
-    log("   test .add single string comma delimited parameter OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(paramsAsCommaDelimitedString.date, new Date(1900, 0, 2, 2, 30), `.add single string comma delimited parameter`, deepStrictEqual);
 
   const multipleParams = xDate(new Date(1900, 0, 1)).add("1 day", "2 hours", "30 minutes");
   log(`\nconst multipleParams = xDate(new Date(1900, 0, 1)).add("1 day", "2 hours", "30 minutes");\n     => ${multipleParams.value}`);
-
-  try {
-    assert.deepEqual(multipleParams.date, new Date(1900, 0, 2, 2, 30), ".add multiple parameters");
-    log("   test .add multiple parameters OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(multipleParams.date, new Date(1900, 0, 2, 2, 30), `.add multiple parameters`, deepStrictEqual);
 
   const invalidParams = xDate(new Date(1900, 0, 1)).add("1 millennium");
   log(`\nconst invalidParams = xDate(new Date(1900, 0, 1)).add("1 millennium");\n     => ${invalidParams.value}`);
-
-  try {
-    assert.deepEqual(invalidParams.date, new Date(1900, 0, 1), ".add invalid parameters");
-    log("   test .add invalid parameters OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(invalidParams.date, new Date(1900, 0, 1), `.add invalid parameters`, deepStrictEqual);
 
   const noParams = xDate(new Date(1900, 0, 1)).add();
   log(`\nconst noParams = xDate(new Date(1900, 0, 1)).add();\n     => ${noParams.value}`);
-
-  try {
-    assert.deepEqual(noParams.date, new Date(1900, 0, 1), ".add no parameters");
-    log("   test .add no parameters OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(noParams.date, new Date(1900, 0, 1), `.add no parameters`, deepStrictEqual);
 
   log(`\n--- miscellaneous ---`);
-  log(`\n\`\${dsr\}\` => ${dsr}`);
 
-  try {
-    assert.strictEqual(`${dsr}`, formatter(dsr), "dsr toString (formatter in instance)");
-    log("   test dsr toString (formatter in instance) OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  log(`\n\`\${dsr\}\` => ${dsr}`);
+  runTest(`${dsr}`, formatter(dsr), `dsr toString (formatter in instance)`);
 
   const tsInstance = xDate();
-
   log(`\n\`\${xDate()}\ => value: ${tsInstance}`);
-
-  try {
-    assert.strictEqual(`${tsInstance}`, formatter(tsInstance), "toString no instance formatter");
-    log("   test toString no instance formatter OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(`${tsInstance}`, formatter(tsInstance), `toString no instance formatter`);
 
   const test = xDate(new Date(1900, 0, 1), "this is not a function");
   log(`\nconst test = xDate(new Date(1900, 0, 1), "this is not a function"); 
      => ${test.value}`);
-
-  try {
-    assert.deepStrictEqual(test.value, new Date(1900, 0, 1), "instantiation bad formatter");
-    log("   test instantiation bad formatter OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(test.value, new Date(1900, 0, 1), `instantiation bad formatter`, deepStrictEqual);
 
   const formatterTest = test.clone(d => d.toLocaleString(`de-DE`));
   log(`\nconst formatterTest = test.clone(d => d.toLocaleString("de-DE"))); => ${formatterTest.value}`);
-
-  try {
-    assert.strictEqual(formatterTest.value, formatterTest.date.toLocaleString("de-DE"), "clone with new formatter");
-    log("   test clone with new formatter OK");
-  } catch (err) {
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(formatterTest.value, formatterTest.date.toLocaleString("de-DE"), `clone with new formatter`);
 
   log(`\nfactory call without extensions => factory()`);
-  try {
-    assert.doesNotThrow(factory(), "initialize without extensions does not throw");
-    log("   initialize without extensions does not throw OK");
-  } catch (err) {
-    console.log(`WTF`);
-    errors = true;
-    log(`   ERROR for test ${err.message}${getLine(err)}   expected: ${err.expected},\n     actual: ${err.actual}\n`);
-  }
+  runTest(factory, ``, `initialize without extensions does not throw`, doesNotThrow);
 
-  log(errors ? `\nAll done, but there were errors ...\n` : `\nAll done!\n`);
+  log(errors ? `\nAll done, but there were errors ...\n` : `\nAll ${cnt} tests are ok!\n`);
   logIt(...logs);
 }
 
